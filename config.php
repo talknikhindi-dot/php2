ce860213402eab2eca61662a2c80dd5ec2d7350e
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-10 16:31:34 +0530

    Finalized Cloudinary integration via terminal

--- config.php
+++ config.php
@@ -3,9 +3,11 @@
 $sql_url = "postgresql://neondb_owner:npg_FJSKj3ei1Zom@ep-wispy-dust-amiz86q5.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require";
 $redis_url = "https://elegant-sole-81336.upstash.io";
 $redis_token = "AbYQAAIjcDEyYmU4YmY0N2YxNDQ0OGFhYmY2ZDA0YjYyZGE4ZjUxNHAxMA";
+
+// Cloudinary Master Credentials
 $cloudinary_name = "defb96dno";
 $cloudinary_key = "287268785168696";
-$cloudinary_secret = "???_?????_???????_????"; 
+$cloudinary_secret = "v7f_P_y9uUqKOfN8U56N4-rUoYo"; // ??????? ??????? ???????? Secret Key
 
-echo "Talknik Master Backend: All Systems Online!";
+echo "Talknik Master Backend: All Systems Online and Connected!";
 ?>
